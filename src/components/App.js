0d347bc2a5cf1b15575c2b89f0f42a874ca4fd8e
shipwoli <khalwalesaviour@gmail.com>
2023-03-28 21:07:54 +0300

    Added a login page

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./Login";
+
+
+export default function App(){
+return(
+
+  <BrowserRouter>
+  <Login/>
+  <Routes>
+<Route path ="/login" element ={<Login/>}/>
+  </Routes>
+  
+  </BrowserRouter>
+
+)
+
+}
