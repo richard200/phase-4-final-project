6429b2c8060d815f96b0e5ee05e053a1d2e7b1a7
shipwoli <khalwalesaviour@gmail.com>
2023-03-28 21:34:59 +0300

    Added create navbar

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./Login";
 import CreateRecipe from "../Recipe/CreateRecipe";
+import Navigation from "./Navbar";
 
 
 export default function App(){
 return(
 
   <BrowserRouter>
-  <Login/>
+  <Navigation/>
   <Routes>
 <Route path ="/login" element ={<Login/>}/>
+<Route path = "/addrecipe" element ={<CreateRecipe/>}/>
   </Routes>
   
   </BrowserRouter>
---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="md">
+      <Container>
+        <Navbar.Brand href="/">Recipe App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+            <Link to="/addrecipe" className="nav-link">
+              Create Recipe
+            </Link>
+           
+            <Link to="/delete-recipe" className="nav-link">
+              Delete Recipe
+            </Link>
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;
