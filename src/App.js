0e2a52284518d4f9d3798a451effced3e63b415e
richard200 <richmn200@gmail.com>
2023-03-29 21:40:58 +0300

    working on signup and about components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {  Switch, Route, Routes} from 'react-router-dom';
+import SignUp from './components/Signup';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div style={{ 
+    //   backgroundImage: 'url("https://richie-ngeti.000webhostapp.com/images/recipe.jpeg")',
+    //  backgroundSize: 'cover',
+    //  backgroundPosition: ' center',
+    //  height: '400px'
+    }}className="App">
+  {/* <SignUp/> */}
+  {/* <Home/> */}
+  <About/>
     </div>
+ 
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "../Footer.css";
+
+function Footer() {
+  return (
+    <div className="main-footer">
+      <div className="container">
+        <div className="row ">
+        <div className="col">
+                <h2>About Us</h2>
+                    <li href="#">Story</li>
+                    <li href="#">Clients</li>
+                    <li href="#">Testimonials</li>
+                    </div>
+                    <div className="col">
+                <h2>Services</h2>
+                    <li href="#">Marketing</li>
+                    <li href="#">Consulting</li>
+                    <li href="#">Development</li>
+                    <li href="#">Design</li>
+               </div>
+               <div className="col">
+                <h2>Contact Us</h2>
+                    <li href="#">United States</li>
+                    <li href="#">United Kingdom</li>
+                    <li href="#">Australia</li>
+                    <li href="#">Support</li>
+               </div>
+               <div className="col">
+                <h2>Social</h2>
+                    <li href="#" className="fab fa-facebook-f">Facebook</li>
+                    <li href="#" className="fab fa-instagram">Instagram</li>
+                    <li href="#"  className="fab fa-youtube" >Youtube</li>
+                    <li href="#" className="fab fa-twitter" >Twitter</li>
+              </div>
+        <hr />
+        <div className="row">
+          <p className="col-sm">
+            &copy;{new Date().getFullYear()} THICC MEMES | All rights reserved |
+            Terms Of Service | Privacy
+          </p>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
