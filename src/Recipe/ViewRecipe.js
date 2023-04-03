67b0fc56deabc276ead3ace05b6addfb93edfee9
richard200 <richmn200@gmail.com>
2023-04-03 16:13:26 +0300

    trying to implement logout

--- src/Recipe/ViewRecipe.js
+++ src/Recipe/ViewRecipe.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Table, Button } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 import Category from '../components/Category';
 const RecipeList = () => {
   const [recipes, setRecipes] = useState([]);
